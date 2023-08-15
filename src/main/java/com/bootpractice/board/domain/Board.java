b591c92d7e50ef1bd1af3c21fa32095db42e088c
gyahury <rudgns1243@naver.com>
2023-08-15 17:46:04 +0900

    [SBB-2] Feat: 엔티티 추가

---
+++ src/main/java/com/bootpractice/board/domain/Board.java
@@ -0,0 +1,27 @@
+package com.bootpractice.board.domain;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "board")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    @Column(name = "user_id")
+    private String userId;
+    @CreationTimestamp
+    @Column(name = "regist_date")
+    private LocalDateTime registDate;
+    @UpdateTimestamp
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
+}
---
+++ src/main/java/com/bootpractice/board/domain/Member.java
@@ -0,0 +1,28 @@
+package com.bootpractice.board.domain;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String username;
+    private String nickname;
+    private String password;
+    @CreationTimestamp
+    @Column(name = "regist_date")
+    private LocalDateTime registDate;
+    @UpdateTimestamp
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
+
+}
