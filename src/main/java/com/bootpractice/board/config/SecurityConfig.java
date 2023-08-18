50d0c6da9ee949e979422f23ce9cd39cd8e688ba
gyahury <rudgns1243@naver.com>
2023-08-18 16:37:35 +0900

    [SBB-9] Refactor: security config 리팩토링
    - extends WebSecurityConfigurerAdapter 방식이 deprecated, SecurityFilterChain Bean 주입 방식으로 변경
    - 가입, 로그인 api 허용
    - jwt 사용을 위한 설정 추가

--- src/main/java/com/bootpractice/board/config/SecurityConfig.java
+++ src/main/java/com/bootpractice/board/config/SecurityConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 
     @Bean
@@ -17,23 +20,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("user")
-                .password(passwordEncoder().encode("123123"))
-                .roles("USER");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable() // http basic 방식은 사용하지 않고 jwt를 사용할 것임
+                .csrf().disable() // 세션이 아니기 때문에 csrf 취약점 보호는 하지 않음
+                .cors() // cors 설정
+                .and()  // 다음 설정으로
+                .authorizeRequests() // 요청 authorize 설정을 시작
+                .antMatchers("/api/members/join","/api/members/login").permitAll() // 가입과 로그인은 authorize없이 허용
+                .antMatchers("/api/**").authenticated() // 위 요청을 제외한 api 요청은 항상 인증이 필요
+                .and() // 다음 설정으로
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //jwt stateless 방식을 사용하기위해 설정
+                .and() // 다음 설정으로
+                //.addFilterBefore(new JwtTokenFilter(memberService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
